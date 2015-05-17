727e027fa37f4c731b098bac2804cfa9cf461c70
FelixOngati <fegati02@gmail.com>
2015-05-17 13:25:31 +0300

    added session beans

--- src/java/controller/ControllerServelet.java
+++ src/java/controller/ControllerServelet.java
@@ -7,11 +7,13 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import session.CategoryFacade;
 
 /**
  *
@@ -25,6 +27,14 @@ import javax.servlet.http.HttpServletResponse;
                            "/purchase",
                            "/chooseLanguage"})
 public class ControllerServelet extends HttpServlet {
+    @EJB
+    private CategoryFacade categoryFacade;
+    
+    @Override
+    public void init() throws ServletException{
+        //store category list in servelet context
+        getServletContext().setAttribute("categories", categoryFacade.findAll());
+    }
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
